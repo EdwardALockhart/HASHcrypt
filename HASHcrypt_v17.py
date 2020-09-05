cbf770f987c28f63bd3a4c965d31be26db32b38c
EdwardALockhart <54292405+EdwardALockhart@users.noreply.github.com>
2020-09-05 10:48:21 +0100

    Add files via upload

--- HASHcrypt_v17.py
+++ HASHcrypt_v17.py
@@ -5,17 +5,17 @@ Created By:
     Edward Lockhart
     September 2020
 
+Description:
+    Encrypts and decrypts messages using a character-value dictionary controlled
+    by a user-defined random state and a securely-random decryption key unique
+    to each encrypted message.
+
 Multi-Key One-Time Pad Cipher:
     Key1: Random state dictating the character-value mapping
     Key2: Randomly generated alphanumeric decryption key
     Operation: XOR bit encryption
-
-Description:
-    Encrypts and decrypts messages using a character-value dictionary controlled
-    by a user-defined random state and a secure randomly generated decryption key
-    unique to each encrypted message.
-    
-Usage:
+  
+Strengths:
     The resulting ciphertext will be impossible to decrypt as long as:
 
     The key is truly random (achieved in code).
@@ -23,6 +23,24 @@ Usage:
     The key is never reused in whole or in part (achieved in code).
     The key and random state are kept completely secret.
 
+Usage:
+    Encryption:
+    Define a reproducible random state which dictates the character-value mapping
+    Enter a plaintext message
+    A securely-random key is generated
+    The character-value mapping converts these into list of integers
+    XOR bitwise operation between these lists produces the cipher values
+    The cipher values are joined into a list as the encrypted message
+    The script returns the encrypted message and unique key
+    
+    Decryption:
+    Enter random state used to encrypt the original message
+    Enter the encrypted message
+    Enter the corresponding unique key
+    The character-value mapping converts the key into a list of integers
+    XOR bitwise operation between these lists produces the message values
+    The message values are converted into characters by the character-value mapping
+    The script returns the decrypted message
 '''
 
 #-------------------------------------------------------------------------------
